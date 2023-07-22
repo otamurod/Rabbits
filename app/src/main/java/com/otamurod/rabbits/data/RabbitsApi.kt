989759d4febd1f2c558d5373b880a747dfbc6a28
otamurod <o.safarov@student.inha.uz>
2023-07-22 10:57:24 +0500

    feat: create app module

--- app/src/main/java/com/otamurod/rabbits/data/RabbitsApi.kt
+++ app/src/main/java/com/otamurod/rabbits/data/RabbitsApi.kt
@@ -5,4 +5,8 @@ import retrofit2.http.GET
 interface RabbitsApi {
     @GET("random-rabbit")
     suspend fun getRandomRabbit(): Rabbit
+
+    companion object{
+        const val BASE_URL = "http://192.168.43.89:8080"
+    }
 }
---
+++ app/src/main/java/com/otamurod/rabbits/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.otamurod.rabbits.di
+
+import com.otamurod.rabbits.data.RabbitsApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideRabbitsApi(): RabbitsApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(RabbitsApi.BASE_URL)
+            .build()
+            .create(RabbitsApi::class.java)
+    }
+}
